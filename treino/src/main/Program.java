b8f14a13bcf3a006b11348c924dd87a4f191adcd
Lucas <lucasangelloti@gmail.com>
2025-07-31 09:14:33 -0300

    segundo commit
    irei trabalhar e depois volto para tratar as exceções

--- treino/src/main/Program.java
+++ treino/src/main/Program.java
@@ -1,6 +1,34 @@
 package main;
 
 
-public class Program {
+import model.entities.Account;
 
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            System.out.println("Enter account data: ");
+            System.out.print("Number: ");
+            int number = sc.nextInt();
+            System.out.print("Holder: ");
+            sc.nextLine();
+            String holder = sc.nextLine();
+            System.out.print("Initial balance: ");
+            double balance = sc.nextDouble();
+            System.out.print("Withdraw limit: ");
+            double withdrawLimit = sc.nextDouble();
+
+            Account account = new Account(number, holder, balance, withdrawLimit);
+
+            System.out.println("Enter amount for withdraw: ");
+            double amount = sc.nextDouble();
+
+        }catch(RuntimeException e){
+            System.out.println("Unexpected error!" + e.getMessage());
+        }
+
+    }
 }
